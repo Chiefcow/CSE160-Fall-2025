391d98ac3b599d794003de81e1fc52538ef588dd
Chiefcow <ngs2021@outlook.com>
2025-12-01 10:03:51 -0800

    tcp test scripts

---
+++ tcptest2.py
@@ -0,0 +1,217 @@
+#ANDES Lab - University of California, Merced
+#Author: UCM ANDES Lab
+#$Author: abeltran2 $
+#$LastChangedDate: 2014-08-31 16:06:26 -0700 (Sun, 31 Aug 2014) $
+#! /usr/bin/python
+import sys
+from TOSSIM import *
+from CommandMsg import *
+
+class TestSim:
+    moteids=[]
+    # COMMAND TYPES
+    CMD_PING = 0
+    CMD_NEIGHBOR_DUMP = 1
+    CMD_ROUTE_DUMP=3
+    CMD_TEST_CLIENT=4
+    CMD_TEST_SERVER=5
+
+    # CHANNELS - see includes/channels.h
+    COMMAND_CHANNEL="command";
+    GENERAL_CHANNEL="general";
+
+    # Project 1
+    NEIGHBOR_CHANNEL="neighbor";
+    FLOODING_CHANNEL="flooding";
+
+    # Project 2
+    ROUTING_CHANNEL="routing";
+
+    # Project 3
+    TRANSPORT_CHANNEL="transport";
+
+    # Personal Debuggin Channels for some of the additional models implemented.
+    HASHMAP_CHANNEL="hashmap";
+
+    # Initialize Vars
+    numMote=0
+
+    def __init__(self):
+        self.t = Tossim([])
+        self.r = self.t.radio()
+
+        #Create a Command Packet
+        self.msg = CommandMsg()
+        self.pkt = self.t.newPacket()
+        self.pkt.setType(self.msg.get_amType())
+
+    # Load a topo file and use it.
+    def loadTopo(self, topoFile):
+        print 'Creating Topo!'
+        # Read topology file.
+        topoFile = 'topo/'+topoFile
+        f = open(topoFile, "r")
+        self.numMote = int(f.readline());
+        print 'Number of Motes', self.numMote
+        for line in f:
+            s = line.split()
+            if s:
+                print " ", s[0], " ", s[1], " ", s[2];
+                self.r.add(int(s[0]), int(s[1]), float(s[2]))
+                if not int(s[0]) in self.moteids:
+                    self.moteids=self.moteids+[int(s[0])]
+                if not int(s[1]) in self.moteids:
+                    self.moteids=self.moteids+[int(s[1])]
+
+    # Load a noise file and apply it.
+    def loadNoise(self, noiseFile):
+        if self.numMote == 0:
+            print "Create a topo first"
+            return;
+
+        # Get and Create a Noise Model
+        noiseFile = 'noise/'+noiseFile;
+        noise = open(noiseFile, "r")
+        for line in noise:
+            str1 = line.strip()
+            if str1:
+                val = int(str1)
+            for i in self.moteids:
+                self.t.getNode(i).addNoiseTraceReading(val)
+
+        for i in self.moteids:
+            print "Creating noise model for ",i;
+            self.t.getNode(i).createNoiseModel()
+
+    def bootNode(self, nodeID):
+        if self.numMote == 0:
+            print "Create a topo first"
+            return;
+        self.t.getNode(nodeID).bootAtTime(1333*nodeID);
+
+    def bootAll(self):
+        i=0;
+        for i in self.moteids:
+            self.bootNode(i);
+
+    def moteOff(self, nodeID):
+        self.t.getNode(nodeID).turnOff();
+
+    def moteOn(self, nodeID):
+        self.t.getNode(nodeID).turnOn();
+
+    def run(self, ticks):
+        for i in range(ticks):
+            self.t.runNextEvent()
+
+    # Rough run time. tickPerSecond does not work.
+    def runTime(self, amount):
+        self.run(amount*1000)
+
+    # Generic Command
+    def sendCMD(self, ID, dest, payloadStr):
+        self.msg.set_dest(dest);
+        self.msg.set_id(ID);
+        self.msg.setString_payload(payloadStr)
+
+        self.pkt.setData(self.msg.data)
+        self.pkt.setDestination(dest)
+        self.pkt.deliver(dest, self.t.time()+5)
+
+    def ping(self, source, dest, msg):
+        self.sendCMD(self.CMD_PING, source, "{0}{1}".format(chr(dest),msg));
+
+    def neighborDMP(self, destination):
+        self.sendCMD(self.CMD_NEIGHBOR_DUMP, destination, "neighbor command");
+
+    def routeDMP(self, destination):
+        self.sendCMD(self.CMD_ROUTE_DUMP, destination, "routing command");
+
+    def cmdTestServer(self, address, port):
+        """
+        Initiates the server at node [address] and binds it to [port].
+        """
+        payloadStr = "{0}{1}".format(chr(address), chr(port))
+        self.sendCMD(self.CMD_TEST_SERVER, address, payloadStr)
+        print "Test Server command sent to node", address, "port", port
+
+    def cmdTestClient(self, source, dest, srcPort, destPort, transfer):
+        """
+        Initiates the client at [source] and connects to [dest]:[destPort].
+        Sends [transfer] bytes of data.
+        """
+        # Pack parameters: dest(1), srcPort(1), destPort(1), transfer(2)
+        payloadStr = "{0}{1}{2}{3}{4}".format(
+            chr(dest), 
+            chr(srcPort), 
+            chr(destPort),
+            chr(transfer & 0xFF),
+            chr((transfer >> 8) & 0xFF)
+        )
+        self.sendCMD(self.CMD_TEST_CLIENT, source, payloadStr)
+        print "Test Client command sent from node", source, "to", dest
+
+    def addChannel(self, channelName, out=sys.stdout):
+        print 'Adding Channel', channelName;
+        self.t.addChannel(channelName, out);
+
+    def testServer(self, address):
+        """
+        Starts a test server on the given node address.
+        Server will bind to port 80 by default.
+        """
+        port = 80  # Default port
+        payloadStr = "{0}{1}".format(chr(address), chr(port))
+        self.sendCMD(self.CMD_TEST_SERVER, address, payloadStr)
+        print "Test Server started on node", address
+
+    def testClient(self, clientAddress, serverAddress, srcPort, destPort, transfer):
+        """
+        Starts a test client that connects to a server.
+        """
+        payloadStr = "{0}{1}{2}{3}{4}".format(
+            chr(serverAddress), 
+            chr(srcPort), 
+            chr(destPort),
+            chr(transfer & 0xFF),
+            chr((transfer >> 8) & 0xFF)
+        )
+        self.sendCMD(self.CMD_TEST_CLIENT, clientAddress, payloadStr)
+        print "Test Client started on node", clientAddress, "connecting to", serverAddress
+
+    def clientClose(self, clientAddress, serverAddress, srcPort, destPort):
+        """
+        Closes a client connection.
+        """
+        payloadStr = "{0}{1}{2}{3}".format(
+            chr(serverAddress),
+            chr(srcPort),
+            chr(destPort),
+            chr(0)
+        )
+        print "Close command for node", clientAddress
+
+def main():
+    s = TestSim();
+    # Before we do anything, lets simulate the network off.
+    s.runTime(1);
+    # Load the the layout of the network.
+    s.loadTopo("tuna-melt.topo");
+    # Add a noise model to all of the motes.
+    s.loadNoise("no_noise.txt");
+    # Turn on all of the sensors.
+    s.bootAll();
+    # Add the main channels. These channels are declared in includes/channels.h
+    s.addChannel(s.COMMAND_CHANNEL);
+    s.addChannel(s.GENERAL_CHANNEL);
+    s.addChannel(s.TRANSPORT_CHANNEL);
+    # After sending a ping, simulate a little to prevent collision.
+    s.runTime(300);
+    s.testServer(1);
+    s.runTime(60);
+    s.testClient(4);
+    s.runTime(1);
+    s.runTime(1000);
+
+if __name__ == '__main__':
+    main()
